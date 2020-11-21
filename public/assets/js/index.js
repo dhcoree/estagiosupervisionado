a9144ad07eaf179f56cf5d0dc851b1039f0d9e12
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2020-11-21 14:06:58 -0300

    add tailwind

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,197 +1,169 @@
 // Setting temporary variable
-let toUpdate = null
+let toUpdate = null;
 
-// Getting all products and update table
+// Getting all sales and update table
 async function getAll() {
   try {
-    // Getting form and table HTML elements
-    const form  = document.body.querySelector('form'),
-          tbody = document.body.querySelector('tbody')
-
-    // Getting form inputs
-    const inputs = Array.from(form.querySelectorAll('input'))
-
-    // Create product payload for request
-    const product = {}
-
     // Mount URL Request
-    let urlRequest = `${location.origin}/api/product`
-    // Variable to verify if is first loop
-    let first = true
-
-    // Get all inputs vlaues and adding to URL Request query
-    for (const input of inputs) {
-      if (first) urlRequest += `?${input.name}=${input.value}`
-      else urlRequest += `&${input.name}=${input.value}`
-
-      first = false
-    }
+    let urlRequest = `${location.origin}/api/venda`;
 
     // Making request
-    const response = await fetch(urlRequest)
+    const response = await fetch(urlRequest);
     // Extract JSON
-    const json = await response.json()
+    const json = await response.json();
 
     // Verify if request has error
-    if (!json.success)
-      throw json.error
+    if (!json.success) throw json.error;
 
     // Clean table rows
-    tbody.innerHTML = ''
+    tbody.innerHTML = "";
 
     // Mounting table rows
-    let rows = ''
-    for (const product of json.data) {
+    let rows = "";
+    for (const sale of json.data) {
       rows += `
         <tr>
-          <td class="description">${product.description}</td>
-          <td class="price">${parseFloat(product.price).toFixed(2)}</td>
-          <td class="options">
-            <button onclick="update('${product._id}', '${product.description}', ${product.price})">Edit</button>
-            <button onclick="remove('${product._id}')">Delete</button>
-          </td>
+          <td class="cliente">${sale.cliente}</td>
+          <td class="produto">${sale.produto}</td>
+          <td class="quantidade">${parseInt(sale.quantidade)}</td>
+          <td class="total">${parseInt(sale.total).toFixed(2)}</td>
         </tr>
-      `
+      `;
     }
 
     // Mounting table rows
-    tbody.innerHTML = rows
+    tbody.innerHTML = rows;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-// Set product to update
+// Set sale to update
 function update(_id, description, price) {
   // Getting Cancel Update button
-  const button = document.querySelector('#cancel_update')
+  const button = document.querySelector("#cancel_update");
 
   // Remove hide button CSS class
-  button.classList.remove('hidden')
+  button.classList.remove("hidden");
 
-  // Saving product to temporary variable
+  // Saving sale to temporary variable
   toUpdate = {
     _id,
     description,
-    price
-  }
+    price,
+  };
 
   // Getting form and inputs
-  const form   = document.querySelector('form'),
-        inputs = Array.from(form.querySelectorAll('input'))
+  const form = document.querySelector("form"),
+    inputs = Array.from(form.querySelectorAll("input"));
 
   // Setting input values
   for (const input of inputs) {
-    if (input.name == 'description') input.value = description
-    else if (input.name == 'price') input.value = parseFloat(price)
+    if (input.name == "description") input.value = description;
+    else if (input.name == "price") input.value = parseFloat(price);
   }
 }
 
-// Delete product
+// Delete sale
 async function remove(id) {
   try {
     // Confirm delete
-    if (confirm('Confirm Delete?')) {
+    if (confirm("Confirm Delete?")) {
       // Mount request options
       const options = {
-        method: 'DELETE'
-      }
+        method: "DELETE",
+      };
 
       // Making request and getting json
-      const response = await fetch(`${location.origin}/api/product/${id}`, options),
-            json     = await response.json()
+      const response = await fetch(
+          `${location.origin}/api/sale/${id}`,
+          options
+        ),
+        json = await response.json();
 
       // Verify if request has error
-      if (!json.success)
-        throw json.error
+      if (!json.success) throw json.error;
 
       // Update table rows
-      getAll()
+      getAll();
     }
   } catch (error) {
-    if (error.message)
-      alert(error.message)
-    else
-      console.log(error)
+    if (error.message) alert(error.message);
+    else console.log(error);
   }
 }
 
 // Submit form
 async function submitForm(event) {
   // Cancel default submit form behaviour
-  event.preventDefault()
+  event.preventDefault();
 
   try {
     // Getting form and inputs
-    const form   = document.querySelector('form'),
-          inputs = Array.from(form.querySelectorAll('input'))
+    const form = document.querySelector("form"),
+      inputs = Array.from(form.querySelectorAll("input"));
 
-    // Create product object payload
-    const product = {}
+    // Create sale object payload
+    const sale = {};
 
-    // Setting product payload properties
+    // Setting sale payload properties
     for (const input of inputs) {
-      product[input.name] = input.value
+      sale[input.name] = input.value;
     }
 
     // Mount request options
     const options = {
-      method: !toUpdate ? 'POST' : 'PUT',
+      method: !toUpdate ? "POST" : "PUT",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(product)
-    }
+      body: JSON.stringify(sale),
+    };
 
     // Create URL Request
-    let urlRequest = `${location.origin}/api/product`
+    let urlRequest = `${location.origin}/api/sale`;
 
     // Verify if it is update
-    if (toUpdate)
-      urlRequest += `/${toUpdate._id}`
+    if (toUpdate) urlRequest += `/${toUpdate._id}`;
 
     // Making request and getting json
     const response = await fetch(urlRequest, options),
-          json     = await response.json()
+      json = await response.json();
 
     // Verify if request has error
-    if (!json.success)
-      throw json.error
+    if (!json.success) throw json.error;
 
     // Verify if it is update
-    if (toUpdate)
-      cancelUpdate()
+    if (toUpdate) cancelUpdate();
 
     // Reset form
-    form.reset()
+    form.reset();
     // Update table rows
-    getAll()
+    getAll();
   } catch (error) {
-    if (error.message)
-      alert(error.message)
-    else
-      console.log(error)
+    if (error.message) alert(error.message);
+    else console.log(error);
   }
 }
 
 function cancelUpdate() {
   // Getting cancel update button and form HTML elements
-  const button = document.querySelector('#cancel_update'),
-        form   = document.querySelector('form')
+  const button = document.querySelector("#cancel_update"),
+    form = document.querySelector("form");
 
   // Remove hidden CSS class from button
-  button.classList.add('hidden')
+  button.classList.add("hidden");
   // Reset form
-  form.reset()
+  form.reset();
 
   // Clean temporary variable
-  toUpdate = null
+  toUpdate = null;
 }
 
 // Getting form
-const form = document.body.querySelector('form')
+const form = document.body.querySelector("form");
 
 // Add submit event to form
-form.addEventListener('submit', submitForm)
+form.addEventListener("submit", submitForm);
 // Update table rows
-getAll()
+getAll();
